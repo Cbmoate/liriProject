f73a7d208d199b383aa76278cc1e26a442cd1b74
CB <cbmoate@gmail.com>
2016-01-19 16:17:51 -0500

    added switch case

--- liri.js
+++ liri.js
@@ -2,7 +2,23 @@
 var fs = require("fs");
 var keys = require("./keys.js");
 
-my-tweets
-spotify-this-song
-move-this
-do-what-it-says
+//uses a swicth case to make functions run
+function runProgram(params){
+  switch(params[0]){
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    case "my-tweets":
+      myTweets();
+      break;
+    case "movie-this":
+      movieThis();
+      break;
+    case "spotify-this-song":
+      spotifyThisSong();
+      break;
+    default:
+      console.log("Invalid command.");
+      break;
+  }
+}
