432f6bdcd443c9446f57d129dbc1263f4992b910
CB <cbmoate@gmail.com>
2016-01-20 10:39:37 -0500

    added twitter keys to liri

--- liri.js
+++ liri.js
@@ -1,9 +1,21 @@
-//declaring variables
-var fs = require("fs");
+//declaring variables/bringing in npms
+var params = process.argv.slice(2);
 var keys = require("./keys.js");
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+//grabbing all the twitter keys 
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret 
+});
+
 
 //uses a swicth case to make functions run
-function runProgram(params){
+function liri(params){
   switch(params[0]){
     case "do-what-it-says":
       doWhatItSays();
