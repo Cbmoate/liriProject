489f889a0610d2a9436c57952b566d2f3a8ff9f1
CB <cbmoate@gmail.com>
2016-01-20 14:41:21 -0500

    spotify added

--- liri.js
+++ liri.js
@@ -29,7 +29,7 @@ function liri(params){
       movieThis();
       break;
     case "spotify-this-song":
-      spotifyThisSong();
+      spotifySong();
       break;
     default:
       console.log("Invalid command.");
@@ -64,7 +64,8 @@ function movieThis(){
         })
       }
     })
-  }else{
+  }
+  else{
     request("http://www.omdbapi.com/?t=" + params[1] + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
       if (!error && response.statusCode == 200) {
         movieInfo = "Title: " + JSON.parse(body).Title + "\n" + "Year: " + JSON.parse(body).Year + "\n" + "IMDB rating: " + JSON.parse(body).imdbRating + "\n" + "Country: " + JSON.parse(body).Country + "\n" + "Language: " + JSON.parse(body).Language + "\n" + "Plot: " + JSON.parse(body).Plot + "\n"+ "Actors: " + JSON.parse(body).Actors + "\n" + "Rotten Tomatoes Rating:" + JSON.parse(body).tomatoRating + "\n" + "Rotten Tomatoes URL:" + JSON.parse(body).tomatoURL +"\n";
@@ -76,5 +77,38 @@ function movieThis(){
   }
 }
 
+//grab spotify info
+function spotifySong(){
+  if (params[1] === undefined){
+    spotify.search({type: "track", query: "What's My Age Again?" }, function(err, data) {
+      if ( err ) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      else{
+        spotifyInfo = "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Listen on Spotify: " + data.tracks.items[0].artists[0].external_urls.spotify +"\n" + "Album: " + data.tracks.items[0].album.name;
+        console.log(spotifyInfo)
+        fs.appendFile("log.txt", spotifyInfo, function(err){
 
+        })
+      }
+    })
+  }
+  else{
+    spotify.search({type: "track", query: params[1] }, function(err, data) {
+      if ( err ) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      else{
+        spotifyInfo = "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Listen on Spotify: " + data.tracks.items[0].artists[0].external_urls.spotify +"\n" + "Album: " + data.tracks.items[0].album.name + "\n";
+        console.log(spotifyInfo)
+        fs.appendFile("log.txt", spotifyInfo, function(err){
+        })
+      }
+    });
+  }
+}
+
+//passes params into the program and runs it
 liri(params)
